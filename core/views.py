a622f9630526348e8cef85a8efe8d7b38b685a35
apanik <ap.anik71@gmail.com>
2021-01-15 19:52:00 +0600

    Template added

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Item, OrderItem, Order
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'Items': Item.objects.all()
+    }
+    return render(request,template_name="home-page.html",context=context)

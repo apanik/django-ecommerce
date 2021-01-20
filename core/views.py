cdd2a0bd85a59b118047d77216e37147ff5d751a
apanik <ap.anik71@gmail.com>
2021-01-20 19:21:15 +0600

    Message related issue solved

--- core/views.py
+++ core/views.py
@@ -57,7 +57,9 @@ def remove_from_cart(request, slug):
     order_qs = Order.objects.filter(
         user=request.user,
         ordered=False)
+    print(order_qs)
     if order_qs.exists():
+        print("getting...")
         order = order_qs[0]
         if order.items.filter(item__slug=item.slug).exists():
             order_item = OrderItem.objects.filter(
@@ -68,6 +70,7 @@ def remove_from_cart(request, slug):
             order.items.remove(order_item)
             order_item.delete()
             messages.info(request, "This item was removed from your cart.")
+            print(messages)
             return redirect("ItemDetailView", slug=slug)
         else:
             messages.info(request, "This item was not in your cart")
